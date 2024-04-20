02a21db634ebf7a1ef7d6b42e34f94b1c183c435
Erikaa81 <erikafabi_oliveira@hotmail.com>
2024-04-20 18:32:59 -0300

    feat: Adiciona SigIn, UserProfile, as novas rotas e configura login e logout

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,44 @@
-async function signIn(email: string, password: string, onSuccess: () => void) {
-  console.log('will sign in...')
+import { storage } from './storage'
+console.log('will sign in...')
+
+function success(response: Response, onSuccess: () => void) {
+  response.json().then((json) => {
+    storage.store('token', json.token)
+    storage.store('email', json.email)
+    onSuccess()
+  })
+}
+
+function failure(response: Response, onFailure: () => void) {
+  onFailure()
+}
+
+function isLoggedIn() {
+  return Boolean(storage.get('token'))
+}
+
+function signOut(andThen: () => void = () => {}) {
+  storage.remove('token')
+  storage.remove('email')
+
+  andThen()
+}
+
+function currentUser() {
+  if (!isLoggedIn()) {
+    return null
+  }
+  return {
+    email: storage.get('email')
+  }
+}
+
+async function signIn(
+  email: string,
+  password: string,
+  onSuccess: () => void,
+  onFailure: () => void
+) {
   const body = {
     login: {
       email: email,
@@ -14,10 +53,18 @@ async function signIn(email: string, password: string, onSuccess: () => void) {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(body)
+  }).then((response) => {
+    if (response.ok) {
+      success(response, onSuccess)
+    } else {
+      failure(response, onFailure)
+    }
   })
-  response.json().then((json) => onSuccess())
 }
 
 export const auth = {
-  signIn: signIn
+  signIn,
+  isLoggedIn,
+  currentUser,
+  signOut
 }
